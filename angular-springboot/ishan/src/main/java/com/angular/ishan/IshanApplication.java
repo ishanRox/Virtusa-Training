6774bb323457cf2e6dd86cc9528e8af0b095d895
Drone <37543774+ishanRox@users.noreply.github.com>
2020-03-07 08:22:43 +0530

    ishan 1st p added

---
+++ angular-springboot/ishan/src/main/java/com/angular/ishan/IshanApplication.java
@@ -0,0 +1,13 @@
+package com.angular.ishan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IshanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IshanApplication.class, args);
+	}
+
+}
