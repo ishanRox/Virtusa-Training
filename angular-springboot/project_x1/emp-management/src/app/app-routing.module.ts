9842488f31d52d4f338725b8276b1a0c3a386472
Drone <37543774+ishanRox@users.noreply.github.com>
2020-03-08 01:26:55 +0530

    login created

--- angular-springboot/project_x1/emp-management/src/app/app-routing.module.ts
+++ angular-springboot/project_x1/emp-management/src/app/app-routing.module.ts
@@ -2,13 +2,20 @@ import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
 import { EmployeeComponent } from "./employee/employee.component";
 import { AddEmployeeComponent } from './add-employee/add-employee.component';
+import { LoginComponent } from './login/login.component';
+import { LogoutComponent } from './logout/logout.component';
+import { AuthGaurdService } from './service/auth-gaurd.service';
 
 //url added for routing
 //allow angular to show multiple contents depending on which route(url) is chosen
-const routes: Routes = [{ path: "emp", component: EmployeeComponent }
-  , { path: "addemp", component: AddEmployeeComponent }
+const routes: Routes = [
+  { path: "", component: EmployeeComponent, canActivate: [AuthGaurdService] }
+  , { path: "addemp", component: AddEmployeeComponent, canActivate: [AuthGaurdService] }
+  , { path: 'login', component: LoginComponent, canActivate: [AuthGaurdService] }
+  , { path: 'logout', component: LogoutComponent, canActivate: [AuthGaurdService] }
 ];
 
+//format code shift+alt+f
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- angular-springboot/project_x1/emp-management/src/app/app.module.ts
+++ angular-springboot/project_x1/emp-management/src/app/app.module.ts
@@ -9,12 +9,14 @@ import { AddEmployeeComponent } from './add-employee/add-employee.component';
 import { FormsModule } from '@angular/forms';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { LoginComponent } from './login/login.component';
+import { LogoutComponent } from './logout/logout.component';
 
 // What is the purpose of the app.module.ts?
 //     It is to startup your application, and set the links to your other modules.
 
 @NgModule({
-  declarations: [AppComponent, EmployeeComponent, AddEmployeeComponent, HeaderComponent, FooterComponent],
+  declarations: [AppComponent, EmployeeComponent, AddEmployeeComponent, HeaderComponent, FooterComponent, LoginComponent, LogoutComponent],
   imports: [BrowserModule
     , AppRoutingModule
     , HttpClientModule
