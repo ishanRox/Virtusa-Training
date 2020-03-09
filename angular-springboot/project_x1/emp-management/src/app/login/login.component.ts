72754d72ea5d35a600ada5d654da572117da8f94
Drone <37543774+ishanRox@users.noreply.github.com>
2020-03-10 00:40:24 +0530

    sticky footer

--- angular-springboot/project_x1/emp-management/src/app/login/login.component.ts
+++ angular-springboot/project_x1/emp-management/src/app/login/login.component.ts
@@ -22,7 +22,7 @@ export class LoginComponent implements OnInit {
   checkLogin() {
     (this.loginservice.authenticate(this.username, this.password).subscribe(
       data => {
-        this.router.navigate(['/emp'])
+        this.router.navigate(['/quections'])
         this.invalidLogin = false
       },
       error => {
