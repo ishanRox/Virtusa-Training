9aba499ebc972f08c2badc8f814274700057452b
Drone <37543774+ishanRox@users.noreply.github.com>
2020-03-16 08:31:36 +0530

    added#1

--- finalRelease/QuectionP_Admin/src/main/java/com/admin/repository/QRepository.java
+++
@@ -1,8 +0,0 @@
-package com.admin.repository;
-
-import com.admin.model.Quection;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface QRepository extends JpaRepository<Quection,Integer> {
-
-}
