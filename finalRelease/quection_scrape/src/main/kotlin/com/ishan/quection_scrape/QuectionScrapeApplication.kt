1ba3c2b65c4715882d4f6d0b7712728128eed558
Drone <37543774+ishanRox@users.noreply.github.com>
2020-03-25 23:22:29 +0530

    QuectionScrape service added

---
+++ finalRelease/quection_scrape/src/main/kotlin/com/ishan/quection_scrape/QuectionScrapeApplication.kt
@@ -0,0 +1,11 @@
+package com.ishan.quection_scrape
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class QuectionScrapeApplication
+
+fun main(args: Array<String>) {
+	runApplication<QuectionScrapeApplication>(*args)
+}
---
+++ finalRelease/quection_scrape/src/test/kotlin/com/ishan/quection_scrape/QuectionScrapeApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.ishan.quection_scrape
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class QuectionScrapeApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
