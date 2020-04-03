63e5851c411db055d8c9b1be485f4dab36b54e49
Drone <37543774+ishanRox@users.noreply.github.com>
2020-04-03 21:23:59 +0530

    go server steped

---
+++ finalRelease/goserver/hello.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+func main() {
+	fmt.Printf("hello go world !")
+	fmt.Println("the time is", time.Now())
+	fmt.Println("The value is ", rand.Seed)
+}
