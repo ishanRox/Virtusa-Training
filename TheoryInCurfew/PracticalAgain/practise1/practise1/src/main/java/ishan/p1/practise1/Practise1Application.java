6a8fb34766a367425854ffef7480286e19fdd85a
Drone <37543774+ishanRox@users.noreply.github.com>
2020-04-19 16:36:00 +0530

    refixed code

---
+++ TheoryInCurfew/PracticalAgain/practise1/practise1/src/main/java/ishan/p1/practise1/Practise1Application.java
@@ -0,0 +1,13 @@
+package ishan.p1.practise1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Practise1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Practise1Application.class, args);
+	}
+
+}
---
+++ TheoryInCurfew/PracticalAgain/practise1/practise1/src/test/java/ishan/p1/practise1/Practise1ApplicationTests.java
@@ -0,0 +1,13 @@
+package ishan.p1.practise1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Practise1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
