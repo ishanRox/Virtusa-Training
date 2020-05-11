babebc1979871c29913828420696737a2c720da7
ishanRox <roxishan8@gmail.com>
2020-05-11 10:55:47 +0530

    singeleton design added

---
+++ design-patterns/singeleton/singeleton/src/main/java/com/design/singeleton/SingeletonApplication.java
@@ -0,0 +1,13 @@
+package com.design.singeleton;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SingeletonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SingeletonApplication.class, args);
+	}
+
+}
---
+++ design-patterns/singeleton/singeleton/src/test/java/com/design/singeleton/SingeletonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.design.singeleton;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SingeletonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
