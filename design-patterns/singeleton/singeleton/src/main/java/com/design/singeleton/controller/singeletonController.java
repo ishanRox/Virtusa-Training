a7800f6507f115f217e15966c51ed173c9dd455a
ishanRox <roxishan8@gmail.com>
2020-05-11 10:53:35 +0530

    singeleton added

---
+++ design-patterns/singeleton/singeleton/src/main/java/com/design/singeleton/controller/singeletonController.java
@@ -0,0 +1,14 @@
+package com.design.singeleton.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class singeletonController {
+
+    @RequestMapping()
+    public static void getInstance(String[] args) {
+
+    }
+}
