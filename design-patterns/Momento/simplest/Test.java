2ae796c362348619ec69651c9f241f744500425b
ishanRox <roxishan8@gmail.com>
2020-06-19 15:59:09 +0530

    memento lastElement and pop

--- design-patterns/Momento/simplest/Test.java
+++ design-patterns/Momento/simplest/Test.java
@@ -1,4 +1,4 @@
-import Originator.Memento;
+import java.util.Stack;
 
 /**
  * Test
@@ -7,15 +7,44 @@ public class Test {
 
     public static void main(String[] args) {
         System.out.println("memento pattern");
+        Originator originator = new Originator();
+        CareTacker careTacker = new CareTacker();
+
+        originator.setState(100);
+        careTacker.save(originator);
+
+        originator.setState(200);
+        careTacker.save(originator);
+
+        originator.setState(300);
+        careTacker.save(originator);
+
+        System.out.println(originator.getState() + " current state ");
+        careTacker.revert(originator);
+        System.out.println(originator.getState() + " previouse state ");
+        careTacker.revert(originator);
+        System.out.println(originator.getState() + " prviouse state ");
+
+        ;
+
     }
 }
 
 class CareTacker {
+    private Stack<Originator.Memento> history = new Stack();
 
+    public void save(Originator originator) {
+        history.add(originator.saveMementoToCareTackerHistory());
+    }
+
+    public void revert(Originator originator) {
+        history.pop();
+        originator.revertUsingCareTackerMemento(history.lastElement());
+    }
 }
 
 class Originator {
-    int state;
+    private int state;
 
     public void setState(int state) {
         this.state = state;
@@ -29,12 +58,12 @@ class Originator {
         return new Memento(this.state);
     }
 
-    public void revertUsingCaretackerMemento(Memento memento) {
+    public void revertUsingCareTackerMemento(Memento memento) {
         this.state = memento.state;
     }
 
-    public  static class Memento {
-        int state;
+    public static class Memento {
+        private int state;
 
         Memento(int state) {
             this.state = state;
