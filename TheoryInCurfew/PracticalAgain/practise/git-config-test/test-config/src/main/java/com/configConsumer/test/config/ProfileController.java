f5a6938c811c6e478c14ff5e6ab1ccd32c83341d
ishanRox <roxishan8@gmail.com>
2020-07-13 12:14:47 +0530

    thymleaf web site added from data got by git hub

--- TheoryInCurfew/PracticalAgain/practise/git-config-test/test-config/src/main/java/com/configConsumer/test/config/ProfileController.java
+++ TheoryInCurfew/PracticalAgain/practise/git-config-test/test-config/src/main/java/com/configConsumer/test/config/ProfileController.java
@@ -2,17 +2,21 @@ package com.configConsumer.test.config;
 
 import com.configConsumer.test.config.model.MemberProfileConfiguration;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class ProfileController {
     @Autowired
     MemberProfileConfiguration memberProfileConfiguration;
 
     @RequestMapping("/profile")
-    public MemberProfileConfiguration getConfig() {
-        return memberProfileConfiguration;
+    public String getConfig(Model model) {
+        model.addAttribute("model",memberProfileConfiguration.getDefaultModel());
+        model.addAttribute("min",memberProfileConfiguration.getMinRentPeriod());
+        return "ishan";
     }
 
 
