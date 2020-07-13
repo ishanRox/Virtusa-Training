de98f4c0e338e5a007bf8db14ac135c1f297395e
ishanRox <roxishan8@gmail.com>
2020-07-13 12:00:03 +0530

    documentation completed about config

---
+++ TheoryInCurfew/PracticalAgain/practise/git-config-test/test-config/src/main/java/com/configConsumer/test/config/ProfileController.java
@@ -0,0 +1,19 @@
+package com.configConsumer.test.config;
+
+import com.configConsumer.test.config.model.MemberProfileConfiguration;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ProfileController {
+    @Autowired
+    MemberProfileConfiguration memberProfileConfiguration;
+
+    @RequestMapping("/profile")
+    public MemberProfileConfiguration getConfig() {
+        return memberProfileConfiguration;
+    }
+
+
+}
--- TheoryInCurfew/PracticalAgain/practise/git-config-test/test-config/src/main/java/com/configConsumer/test/config/model/MemberProfileConfiguration.java
+++ TheoryInCurfew/PracticalAgain/practise/git-config-test/test-config/src/main/java/com/configConsumer/test/config/model/MemberProfileConfiguration.java
@@ -1,4 +1,20 @@
 package com.configConsumer.test.config.model;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.stereotype.Component;
+
+@Component
 public class MemberProfileConfiguration {
+    @Autowired
+    Environment environment;
+
+    public String getDefaultModel() {
+        return environment.getProperty("vehicle.default.model");
+    }
+
+    //get and names according to bean naming convention
+    public String getMinRentPeriod() {
+        return environment.getProperty("member.rent.min");
+    }
 }
