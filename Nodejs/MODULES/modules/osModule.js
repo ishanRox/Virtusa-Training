b378ca2c1a42549587979bdb24b79daeadd7e200
ishanRox <roxishan8@gmail.com>
2020-08-11 11:45:58 +0530

    node events module started

--- Nodejs/MODULES/modules/osModule.js
+++ Nodejs/MODULES/modules/osModule.js
@@ -1,6 +1,16 @@
-const os= require('os');
+const os = require('os');
 const { log } = require('console');
-const totalMemory=os.totalmem();
-const freeMemory=os.freemem();
+const totalMemory = os.totalmem() / 1024;
+const freeMemory = os.freemem() / 1024;
 
 console.log(`total memory ${totalMemory} free memory ${freeMemory}`);
+
+const fileSystem = require('fs');
+
+console.log(fileSystem.readdirSync('./'));
+
+const readAsync = fileSystem.readdir('./sf', (error, files) => {
+    if (error) console.log('error', error);
+    else console.log('result', files);
+});
+
