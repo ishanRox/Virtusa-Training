3b364214131c43384326096fa7402344b89906d4
ishanRox <roxishan8@gmail.com>
2020-08-19 09:19:46 +0530

    node get

--- Nodejs/express-demo/index.js
+++ Nodejs/express-demo/index.js
@@ -1,14 +1,15 @@
 const express = require('express');
 const app = express();
 
+const courses = [...Array(10).keys()].map(e => { return { name: `course${e}`, id: e }; });
+
+console.log(courses);
 
 //these are end points get post delete update
-
 app.get('/', (req, res) => {
 
     res.send('<h1>hello world</h1>')
 });
-
 
 app.get('/api/courses', (req, res) => {
 
@@ -16,19 +17,20 @@ app.get('/api/courses', (req, res) => {
 });
 
 //get query string parameters- for optional things
-
 app.get(`/api/courses/op`, (req, res) => {
     res.send(req.query);
 });
 
 //Parameters-for essential things 
-
+//Example for get
 app.get(`/api/courses/:id`, (req, res) => {
-    res.send(parseInt(req.params.id) * 100 + " hello ");
+    const course = courses.find(e => e.id === parseInt(req.params.id));
+    if (!course) res.status(404).send('id not found');
+
+    res.send(course);
 });
 
 //Read enviroment variable using port
-
 const port = process.env.PORT || 3000;
 
 app.listen(port, _ => console.log(`listning on port ${port}`));
