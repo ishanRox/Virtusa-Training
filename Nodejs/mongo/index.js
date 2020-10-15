6ca29af83eb8c27f1fd7acf27440c280374ae69d
ishanRox <roxishan8@gmail.com>
2020-10-15 09:55:33 +0530

    updates added 2020 ඔක්තෝබර් 15 වැනි බ්‍රහස්පතින්දා 09:55:32 +0530

--- Nodejs/mongo/index.js
+++ Nodejs/mongo/index.js
@@ -70,6 +70,6 @@ getCourses();
 
 // [...Array(10).keys()].forEach(e => {
 //     console.log(e + 1000);
-//     createCourse(`node${e}`, `ishan${e}`, e * 1000);
+//     createCourse(`node${e}`, `ishan${e}`, e * 1000); 
 // });
 
