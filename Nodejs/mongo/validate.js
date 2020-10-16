4ef50b289e0c2450701b3d0144951ee1ca1bb3c1
ishanRox <roxishan8@gmail.com>
2020-10-16 18:28:12 +0530

    updates added 2020 ඔක්තෝබර් 16 වැනි සිකුරාදා 18:28:11 +0530

---
+++ Nodejs/mongo/validate.js
@@ -0,0 +1,41 @@
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/playground')
+    .then(() => console.log('Connected to MongoDb'))
+    .catch(e => console.error('Couldnt connect to mongo', e));
+
+//Create a schema (like a Table template or blueprint for make documents)
+const courseSchema = new mongoose.Schema(
+    {
+        name: String,
+        author: String,
+        tags: [String],
+        date: { type: Date, default: Date.now },
+        isPublish: Boolean,
+        price: Number
+    }
+);
+
+//this  is like class object made from  above schema  template schema is the template for the 
+//Table(Collection) and  this object is the real instance of it
+//From this object we can create documents(rows) and we can query the all Course table(collection) documents(rows)
+//this is like class 
+const Course = mongoose.model('Course', courseSchema);
+
+//use async await to get data from async function that send data
+async function createCourse(courseName, author, price) {
+
+    //real object (document - row ) that we made from the "Course" class  
+    const course = new Course({
+        name: 'Angular.js course',
+        author,
+        tags: ['Angular', 'backend'],
+        isPublish: true,
+        price
+    });
+
+    const result = await course.save();
+    console.log(result);
+}
+
+ createCourse(`node123`, `ishan roxa`, 21000); 
