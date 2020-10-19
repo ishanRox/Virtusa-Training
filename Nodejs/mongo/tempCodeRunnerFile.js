64cf742c162ab42b4c9daa1b1c897854d3c6fcb1
ishanRox <roxishan8@gmail.com>
2020-10-19 16:10:36 +0530

    updates added 2020 ඔක්තෝබර් 19 වැනි සඳුදා 16:10:33 +0530

--- Nodejs/mongo/tempCodeRunnerFile.js
+++ Nodejs/mongo/tempCodeRunnerFile.js
@@ -1,4 +1,1 @@
-[...Array(10).keys()].forEach(e => {
-    console.log(e + 1000);
-    createCourse(`node${e}`, `ishan${e}`, e * 1000);
-});
+await
--- Nodejs/mongo/validate.js
+++ Nodejs/mongo/validate.js
@@ -1,25 +1,35 @@
+'use strict'
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/playground')
+mongoose.connect('mongodb://localhost/playground', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true
+})
     .then(() => console.log('Connected to MongoDb'))
     .catch(e => console.error('Couldnt connect to mongo', e));
 
 //Create a schema (like a Table template or blueprint for make documents)
 const courseSchema = new mongoose.Schema(
     {
-        name: { type: String, required: true },
+        name: { type: String, required: true, minlength: 5, maxlength: 10, match: /^[a-zA-Z]+$/ },
         author: { type: String, required: true },
-        tags: [String],
-        date: { type: Date, default: Date.now },
-        isPublish: Boolean,
-        price: {
-            type: Number,required:function() {
-                return this.isPublish;
+        tags: {
+            type: [String], required: true,
+            validate: {
+                validator: function (v) {
+                    return v.length > 0;
+                },
+                message: 'A course should have at least one tag !'
             }
-            
-        }
+        },
+        isPublish: { type: Boolean, required: true, default: false },
+        price: { type: Number, required: function (val) { return this.isPublish; } }
     }
 );
+
+
 
 //this  is like class object made from  above schema  template schema is the template for the 
 //Table(Collection) and  this object is the real instance of it
@@ -28,24 +38,23 @@ const courseSchema = new mongoose.Schema(
 const Course = mongoose.model('Course', courseSchema);
 
 //use async await to get data from async function that send data
-async function createCourse(courseName, author, price) {
+async function createCourse(name, author, price) {
 
     //real object (document - row ) that we made from the "Course" class  
     const course = new Course({
-       
-        author,
-        tags: ['Angular', 'backend'],
+        name, author,
+        tags: [],
         isPublish: true,
-
+        price
     });
     try {
-        await Course.validate(course);
-        console.log("ok valid");
-        //        const result = await course.save();
+        const result = await course.validate();
+        console.log("ok valid ");
+        //const result = await course.save();
     } catch (error) {
         console.log(`promise rejected ${error.message}`);
     }
 
 }
 
-createCourse(`node123`, `ishan roxa`, 1000); 
+createCourse(`ishan`, `ishan roxa`, 1); 
