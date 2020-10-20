49ecc6512ce2522c476db8d229b53a48a23b8a96
ishanRox <roxishan8@gmail.com>
2020-10-20 16:14:16 +0530

    updates added 2020 ඔක්තෝබර් 20 වැනි අඟහරුවාදා 16:14:16 +0530

--- Nodejs/mongo/validate.js
+++ Nodejs/mongo/validate.js
@@ -1,5 +1,5 @@
-'use strict'
 const mongoose = require('mongoose');
+ let x=1;
 
 mongoose.connect('mongodb://localhost/playground', {
     useNewUrlParser: true,
@@ -19,6 +19,8 @@ const courseSchema = new mongoose.Schema(
             type: [String], required: true,
             validate: {
                 validator: function (value) {
+                   
+
                     return new Promise((resolve, reject) => {
                         // There are two ways for an promise-based async validator to fail:
                         // 1) If the promise rejects, Mongoose assumes the validator failed with the given error.
@@ -26,13 +28,14 @@ const courseSchema = new mongoose.Schema(
                         //so important - we can check it when test case fails and it reject it goes to the 'new Error(msg)'
                         //but if we give correct result and resolve(false) it gets the '  message: 'msg''
                         setTimeout(() => {
-                            console.log('Getting HTTP validation...');
+                            console.log('Getting HTTP validation...'+x);
                             const result = value && value.length > 0;
                             if (result) {
                                 resolve();
                             } else {
                                 reject(new Error('A course should have at least one tag.'));
                             }
+                            x++;
                         }, 1000);
                     });
                 },
@@ -40,7 +43,7 @@ const courseSchema = new mongoose.Schema(
             }
         },
         isPublish: { type: Boolean, required: true, default: false },
-        price: { type: Number, required: function (val) { return this.isPublish; } }
+        price: { type: Number, required: function (val) { return this.isPublish; },set:v=>Math.round(v),get:v=>Math.round(v) }
     }
 );
 
@@ -62,16 +65,15 @@ async function createCourse(name, author, price) {
         isPublish: true,
         price
     });
+
     try {
-        const result = await course.validate();
+        await course.validate();
         console.log("ok valid ");
         await course.save();
     } catch (error) {
         console.log(`promise rejected ${error.message}`);
-
-
     }
 
 }
 
-createCourse(`    ishan vihanga`, `ishan roxa`, 1); 
+createCourse(`    ishanv `, `ishan roxa`, 111.232); 
